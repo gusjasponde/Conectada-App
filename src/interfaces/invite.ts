0939d85fb5b353ea4a1ab96a2c2b1082cd66f48e
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2017-11-11 02:17:00 -0200

    Add invite interface

---
+++ src/interfaces/invite.ts
@@ -0,0 +1,10 @@
+export interface Invite {
+    city: string,
+    state: string,
+    university: string,
+    campus: string,
+    universityEmail: string,
+    email: string,
+    age: string,
+    course: string
+}
